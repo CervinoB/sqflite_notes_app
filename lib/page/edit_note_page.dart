b3d8f7a0b5e725ab5e52d2cffa797501f6e88a3d
CervinoB <jcervinobarbosa@gmail.com>
2022-12-21 15:26:55 -0300

    changing the deprecated property

--- lib/page/edit_note_page.dart
+++ lib/page/edit_note_page.dart
@@ -61,8 +61,8 @@ class _AddEditNotePageState extends State<AddEditNotePage> {
       padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          onPrimary: Colors.white,
-          primary: isFormValid ? null : Colors.grey.shade700,
+          foregroundColor: Colors.white,
+          backgroundColor: isFormValid ? null : Colors.grey.shade700,
         ),
         onPressed: addOrUpdateNote,
         child: const Text('Save'),
